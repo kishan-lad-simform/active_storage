9fd81728a5bfd017d54a8a448b6ab6b85e3098f4
Kishan lad <kishan.l@simformsolution.com>
2023-05-03 17:01:57 +0530

    TE7-T134:active_storage:Fix:Use Image Processing gem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_one_attached :avatar, service: :local
   validates :name, :email, :contact, :avatar ,presence: true
   validates :email, uniqueness: true
   validates :contact, numericality: { only_integer: true }
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
